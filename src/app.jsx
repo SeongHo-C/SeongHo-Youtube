bb425e467093a3a3dca2b54359d04fe4644b92c4
SeongHo-C <leeseong010@naver.com>
2022-07-21 22:09:40 +0900

    검색 기능 리팩토링

--- src/app.jsx
+++ src/app.jsx
@@ -5,25 +5,13 @@ import React from 'react';
 import VideoList from './components/videoList/video_list';
 import SearchHeader from './components/searchHeader/search_header';
 
-const App = (props) => {
+const App = ({ youtube }) => {
   const [videos, setVideos] = useState([]);
 
   const onSearch = (query) => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyAKRQzsqhH_aeKGXe_XXcBKAe-deOjILyU`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      )
-      .then((items) => setVideos(items))
-      .catch((error) => console.log('error', error));
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
   };
 
   useEffect(() => {
---
+++ src/service/youtube.js
@@ -0,0 +1,20 @@
+class Youtube {
+  constructor(key) {
+    this.key = key;
+    this.getRequestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+  }
+
+  async search(query) {
+    const response = await fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
+      this.getRequestOptions
+    );
+    const result_1 = await response.json();
+    return result_1.items.map((item) => ({ ...item, id: item.id.videoId }));
+  }
+}
+
+export default Youtube;
